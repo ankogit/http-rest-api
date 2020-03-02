d086341a3212b77af5d051612321a151135a924e
Anatoly Antonenko <ya.tolik-a@yandex.ru>
2020-03-02 18:23:37 +0300

    need fix

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,18 +1,15 @@
 package apiserver
 
-import "github.com/ankogit/http-rest-api/internal/app/store"
 
 type Config struct {
 	BingAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level"`
 	DatabaseURL string `toml:"database_url"`
-	Store *store.Config
 }
 
 func NewConfig() *Config  {
 	return &Config{
 		BingAddr:":8080",
 		LogLevel: "debug",
-		Store: store.NewConfig(),
 	}
 }
